7788ad77b01c26d884ceaa2bfcbcdefe303b603f
Lasha Efremidze <efremidzel@hotmail.com>
2016-02-17 22:58:33 -0800

    BACKUP

--- SemiTransparentCustomModal/Sample/Sample/CustomTransition.swift
+++ SemiTransparentCustomModal/Sample/Sample/CustomTransition.swift
@@ -25,6 +25,7 @@ extension CustomTransition: UIViewControllerAnimatedTransitioning {
         let container = transitionContext.containerView()!
 //        let viewControllers: (from: UIViewController, to: UIViewController) = (transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!, transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!)
         let views: (from: UIView, to: UIView) = (transitionContext.viewForKey(UITransitionContextFromViewKey)!, transitionContext.viewForKey(UITransitionContextToViewKey)!)
+//        let views: (from: UIView, to: UIView) = (viewControllers.from.view, viewControllers.to.view)
         let duration = transitionDuration(transitionContext)
         
         let offScreenBottom = CGAffineTransformMakeTranslation(0, container.frame.height)
@@ -35,19 +36,17 @@ extension CustomTransition: UIViewControllerAnimatedTransitioning {
             container.addSubview(views.from)
             container.addSubview(views.to)
         } else {
-            views.to.transform = CGAffineTransformIdentity
             views.to.frame = container.bounds
+            views.to.transform = CGAffineTransformIdentity
             container.addSubview(views.to)
             container.addSubview(views.from)
         }
         
         UIView.animateWithDuration(duration, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: [], animations: {
             if self.presenting {
-                views.from.alpha = 0.5
                 views.to.transform = CGAffineTransformIdentity
             } else {
                 views.from.transform = offScreenBottom
-                views.to.alpha = 1
             }
         }) { _ in
             transitionContext.completeTransition(true)
