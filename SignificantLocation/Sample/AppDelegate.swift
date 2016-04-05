a18c048d51b7c77286091808ac502e22aaf2f455
Lasha Efremidze <efremidzel@hotmail.com>
2016-04-05 00:05:16 -0700

    Added Significant Location

---
+++ SignificantLocation/Sample/AppDelegate.swift
@@ -0,0 +1,68 @@
+//
+//  AppDelegate.swift
+//  Sample
+//
+//  Created by Lasha Efremidze on 3/31/16.
+//  Copyright © 2016 Lasha Efremidze. All rights reserved.
+//
+
+import UIKit
+import SwiftLocation
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        UIApplication.sharedApplication().registerUserNotificationSettings(UIUserNotificationSettings(forTypes: [.Badge, .Sound, .Alert], categories: nil))
+        
+        func fireNotification(object: AnyObject) {
+            let notification = UILocalNotification()
+            notification.fireDate = NSDate()
+            notification.alertBody = "\(object)"
+            notification.soundName = UILocalNotificationDefaultSoundName
+            UIApplication.sharedApplication().scheduleLocalNotification(notification)
+        }
+        
+        try! SwiftLocation.shared.significantLocation({ location in
+            print("Location found \(location)")
+            fireNotification(location!)
+        }, onFail: { error in
+            print("Error \(error)")
+        })
+        
+        if let object = launchOptions?[UIApplicationLaunchOptionsLocationKey] {
+            print("UIApplicationLaunchOptionsLocationKey \(object)")
+        }
+        
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
---
+++ SignificantLocation/Sample/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Lasha Efremidze on 3/31/16.
+//  Copyright © 2016 Lasha Efremidze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
