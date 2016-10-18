aca9e832b01628c3ebde4e84e0498b6cc198360a
Lasha Efremidze <efremidzel@hotmail.com>
2016-10-18 15:03:10 -0700

    adding parallax header

--- ParallaxHeader/Sources/ParallaxHeader.swift
+++ ParallaxHeader/Sources/ParallaxHeader.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-enum Mode {
-    case Fill, TopFill, Top, Center, Bottom
-}
-
 class ParallaxHeader: NSObject {
+    
+    lazy var contentView: UIView = { [unowned self] in
+        let view = ParallaxView()
+        view.parent = self
+        view.clipsToBounds = true
+        return view
+    }()
     
     var scrollView: UIScrollView! {
         didSet {
@@ -22,20 +25,18 @@ class ParallaxHeader: NSObject {
         }
     }
     
-    lazy var contentView: UIView = { [unowned self] in
-        let view = ParallaxView()
-        view.parent = self
-        view.clipsToBounds = true
-        return view
-    }()
-    
-    var view: UIView?
+    var view: UIView! {
+        didSet {
+            updateConstraints()
+        }
+    }
     
     var minimumHeight: CGFloat = 0 {
         didSet {
             layoutContentView()
         }
     }
+    
     var height: CGFloat = 0 {
         didSet {
             adjustScrollViewTopInset(scrollView.contentInset.top - oldValue + height)
@@ -44,10 +45,13 @@ class ParallaxHeader: NSObject {
         }
     }
     
+    enum Mode {
+        case Fill, TopFill, Top, Center, Bottom
+    }
+    
     var mode: Mode = .Fill
     
     func updateConstraints() {
-        guard let view = view else { return }
         view.removeFromSuperview()
         contentView.addSubview(view)
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -104,78 +108,17 @@ private class ParallaxView: UIView {
     
     override func willMoveToSuperview(newSuperview: UIView?) {
         if let view = self.superview as? UIScrollView {
-            view.removeObserver(self.parent, forKeyPath: "contentOffset", context: nil)
+            view.removeObserver(parent, forKeyPath: "contentOffset", context: nil)
         }
     }
     
     override func didMoveToSuperview() {
         if let view = self.superview as? UIScrollView {
-            view.addObserver(self.parent, forKeyPath: "contentOffset", options: .New, context: nil)
+            view.addObserver(parent, forKeyPath: "contentOffset", options: .New, context: nil)
         }
     }
     
 }
-
-//class ScrollViewController: UIViewController {
-//    
-//    var scrollView: ScrollView {
-//        return self.view as! ScrollView
-//    }
-//    
-//    override func loadView() {
-//        super.loadView()
-//        let scrollView = ScrollView()
-//        addObserver(scrollView.parallaxHeader)
-//        self.view = scrollView
-//    }
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        
-//        self.scrollView.parallaxHeader.view = self.headerView;
-//        self.scrollView.parallaxHeader.height = self.headerHeight;
-//        self.scrollView.parallaxHeader.minimumHeight = self.headerMinimumHeight;
-//    }
-//
-//    override func viewDidLayoutSubviews() {
-//        super.viewDidLayoutSubviews()
-//        
-//        scrollView.contentSize = scrollView.frame.size
-//        layoutChildViewController()
-//    }
-//    
-//    deinit {
-//        removeObserver(scrollView.parallaxHeader)
-//    }
-//    
-//    func layoutChildViewController() {
-//        var frame = scrollView.frame
-//        frame.origin = CGPoint()
-//        frame.size.height -= self.scrollView.parallaxHeader.minimumHeight;
-//        self.childViewController.view.frame = frame;
-//    }
-//    
-//}
-//
-//extension ScrollViewController {
-//    
-//    func addObserver(object: AnyObject) {
-//        object.addObserver(self, forKeyPath: "minimumHeight", options: .New, context: nil)
-//    }
-//    
-//    func removeObserver(object: AnyObject) {
-//        object.removeObserver(self, forKeyPath: "minimumHeight", context: nil)
-//    }
-//    
-//    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
-//        if keyPath == "minimumHeight" {
-//            layoutChildViewController()
-//        } else {
-//            super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: context)
-//        }
-//    }
-//    
-//}
 
 class ScrollView: UIScrollView {
     
@@ -213,6 +156,42 @@ class ScrollView: UIScrollView {
     deinit {
         observedViews.forEach { removeObserver($0) }
         removeObserver(self)
+    }
+    
+}
+
+extension ScrollView: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        if self.contentOffset.y > -parallaxHeader.minimumHeight {
+            self.scrollView(self, setContentOffset: CGPoint(x: self.contentOffset.x, y: -parallaxHeader.minimumHeight))
+        }
+    }
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        lock = false
+        observedViews.forEach { removeObserver($0) }
+    }
+    
+}
+
+extension ScrollView: UIGestureRecognizerDelegate {
+    
+    override func gestureRecognizerShouldBegin(gestureRecognizer: UIGestureRecognizer) -> Bool {
+        if let recognizer = gestureRecognizer as? UIPanGestureRecognizer {
+            let velocity = recognizer.velocityInView(self)
+            return abs(velocity.y) > abs(velocity.x)
+        }
+        return true
+    }
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        guard let scrollView = otherGestureRecognizer.view as? UIScrollView where scrollView != self else { return false }
+        if !observedViews.contains(scrollView) {
+            observedViews.insert(scrollView)
+            addObserver(scrollView)
+        }
+        return true
     }
     
 }
@@ -259,42 +238,6 @@ extension ScrollView {
         } else {
             super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: context)
         }
-    }
-    
-}
-
-extension ScrollView: UIGestureRecognizerDelegate {
-    
-    override func gestureRecognizerShouldBegin(gestureRecognizer: UIGestureRecognizer) -> Bool {
-        if let recognizer = gestureRecognizer as? UIPanGestureRecognizer {
-            let velocity = recognizer.velocityInView(self)
-            return abs(velocity.y) > abs(velocity.x)
-        }
-        return true
-    }
-    
-    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-        guard let scrollView = otherGestureRecognizer.view as? UIScrollView where scrollView != self else { return false }
-        if !observedViews.contains(scrollView) {
-            observedViews.insert(scrollView)
-            addObserver(scrollView)
-        }
-        return true
-    }
-    
-}
-
-extension ScrollView: UIScrollViewDelegate {
-    
-    func scrollViewDidScroll(scrollView: UIScrollView) {
-        if self.contentOffset.y > -parallaxHeader.minimumHeight {
-            self.scrollView(self, setContentOffset: CGPoint(x: self.contentOffset.x, y: -parallaxHeader.minimumHeight))
-        }
-    }
-    
-    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
-        lock = false
-        observedViews.forEach { removeObserver($0) }
     }
     
 }
