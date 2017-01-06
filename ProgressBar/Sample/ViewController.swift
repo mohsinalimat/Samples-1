8a7f3dc6e1c78a60fce07f5b2d557eb2d74890e7
Lasha Efremidze <efremidzel@hotmail.com>
2017-01-06 15:36:54 -0800

    updated progress view

--- ProgressBar/Sample/ViewController.swift
+++ ProgressBar/Sample/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var progressBar: ProgressBar! {
+    @IBOutlet weak var progressBar: ProgressView! {
         didSet {
             progressBar.backgroundColor = UIColor(white: 0.9, alpha: 1)
             progressBar.foregroundColor = UIColor(red: 0, green: 0.46, blue: 0.76, alpha: 1)
             progressBar.cornerRadius = progressBar.frame.height / 2
+            progressBar.setProgress(0, animated: false)
         }
     }
     @IBOutlet weak var progressView: UIProgressView! {
@@ -27,61 +28,10 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-//        let deadlineTime = DispatchTime.now() + .seconds(1)
-//        DispatchQueue.main.asyncAfter(deadline: deadlineTime) {
-//            self.progressBar.set(progress: 0.5, animated: true)
-//        }
-    }
-    
-}
-
-class ProgressBar: UIView {
-    
-    private lazy var progressShape: CAShapeLayer = { [unowned self] in
-        let shape = CAShapeLayer()
-        shape.frame = self.bounds
-        shape.backgroundColor = self.tintColor.cgColor
-        shape.cornerRadius = self.cornerRadius
-        self.layer.addSublayer(shape)
-        return shape
-    }()
-    
-    var progress: Float = 0 {
-        didSet {
-            setProgress(progress, animated: false)
+        let deadlineTime = DispatchTime.now() + .seconds(1)
+        DispatchQueue.main.asyncAfter(deadline: deadlineTime) {
+            self.progressBar.setProgress(0.5, animated: true)
         }
-    }
-    
-    override var tintColor: UIColor! {
-        didSet {
-            progressShape.strokeColor = tintColor.cgColor
-        }
-    }
-    
-    var foregroundColor: UIColor? {
-        get { return tintColor }
-        set { tintColor = newValue }
-    }
-    
-    var cornerRadius: CGFloat {
-        get { return layer.cornerRadius }
-        set {
-            layer.cornerRadius = newValue
-            progressShape.cornerRadius = newValue
-        }
-    }
-    
-    func setProgress(_ progress: Float, animated: Bool) {
-        let animation = CASpringAnimation(keyPath: "bounds")
-        animation.fromValue = 0
-        animation.toValue = 1
-        animation.damping = 10
-        animation.duration = 5
-        animation.isRemovedOnCompletion = false
-        animation.fillMode = kCAFillModeForwards
-        progressShape.add(animation, forKey: "bounds")
-        
-//        self.progressShape.frame.size.width = progress * self.bounds.width
     }
     
 }
