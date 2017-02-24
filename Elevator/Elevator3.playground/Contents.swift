10911ce98ca965a6d36c96320e361e97614b938f
Lasha Efremidze <efremidzel@hotmail.com>
2017-02-24 01:34:31 -0800

    backup

--- Elevator/Elevator3.playground/Contents.swift
+++ Elevator/Elevator3.playground/Contents.swift
@@ -31,10 +31,10 @@ func ==(lhs: Passenger, rhs: Passenger) -> Bool {
 
 // Elevator
 struct Elevator {
-    private let min: Floor // min floor
-    private let max: Floor // max floor
-    private var current: Floor // current floor
-    private var up: Bool // direction
+    let min: Floor // min floor
+    let max: Floor // max floor
+    private(set) var current: Floor // current floor
+    private(set) var up: Bool // direction
     private(set) var requests = Set<Passenger>() // requests
     private(set) var passengers = Set<Passenger>() // passengers
     
@@ -50,9 +50,8 @@ struct Elevator {
     }
     
     mutating func run() {
-        while !requests.isEmpty || !passengers.isEmpty {
+        while true {
             print("current floor: \(current)")
-            
             print("requests: \(requests.count), passengers: \(passengers.count)")
             
             // pick up passengers waiting on the current floor if in the same direction
@@ -68,6 +67,11 @@ struct Elevator {
                 passengers.remove($0)
             }
             
+            // stop elevator if no requests or passengers
+            if requests.isEmpty && passengers.isEmpty {
+                break
+            }
+            
             // all floors queued
             let queued = (requests.flatMap { $0.from } + passengers.flatMap { $0.to })
             
@@ -78,6 +82,7 @@ struct Elevator {
                 current -= 1 // move down
             } else {
                 up = !up // change direction
+                print("changed direction: \(up ? "up" : "down")")
             }
         }
     }
@@ -85,68 +90,105 @@ struct Elevator {
 
 // Tests
 class TestCase: XCTestCase {
+    
+    // Elevator should be able to work for any number of floors (specified when the elevator is first installed)
     func testAssertions() {
-        var elevator = Elevator(floors: 4)
-        // take passengers to their requested floor
-        elevator.add(Passenger(id: 0, from: 1, to: 2))
-        elevator.run()
-        
-        XCTAssertTrue(elevator.requests.isEmpty, "Passengers still waiting")
-        XCTAssertTrue(elevator.passengers.isEmpty, "Passengers still inside elevator")
+        let elevator = Elevator(floors: 4)
+        XCTAssertEqual(elevator.max, 4, "Should be 4 floors")
     }
     
+    // Elevator should start on the 1st floor
+    func testAssertions1() {
+        let elevator = Elevator(floors: 4)
+        XCTAssertEqual(elevator.current, 1, "Should start at 1st floor")
+    }
+    
+    // A passenger should be able to request a specific floor to go to
     func testAssertions2() {
-        var elevator = Elevator(floors: 4)
-        // take passengers to their requested floor
-        elevator.add(Passenger(id: 0, from: 1, to: 3))
-        elevator.add(Passenger(id: 1, from: 2, to: 1))
-        elevator.run()
-        
-        XCTAssertTrue(elevator.requests.isEmpty, "Passengers still waiting")
-        XCTAssertTrue(elevator.passengers.isEmpty, "Passengers still inside elevator")
+        let passenger = Passenger(id: 0, from: 1, to: 2)
+        XCTAssertEqual(passenger.to, 2, "Should request floor 2")
     }
     
+    // A passenger should be able to make the elevator go to the next requested floor
     func testAssertions3() {
         var elevator = Elevator(floors: 4)
-        // take passengers to their requested floor
-        elevator.add(Passenger(id: 0, from: 4, to: 3))
-        elevator.add(Passenger(id: 1, from: 1, to: 2))
+        elevator.add(Passenger(id: 0, from: 1, to: 2))
+        elevator.run()
+        XCTAssertEqual(elevator.current, 2, "Should make elevator go to floor 2")
+    }
+    
+    // A passenger should be able to check what floor the elevator is currently on
+    func testAssertions4() {
+        var elevator = Elevator(floors: 4)
+        elevator.add(Passenger(id: 0, from: 1, to: 2))
+        elevator.run()
+        XCTAssertEqual(elevator.current, 2, "Should be on floor 2")
+    }
+    
+    // Multiple passengers should be able to request different floors
+    func testAssertions5() {
+        var elevator = Elevator(floors: 4)
+        elevator.add(Passenger(id: 0, from: 1, to: 2))
+        elevator.add(Passenger(id: 1, from: 4, to: 3))
         elevator.add(Passenger(id: 2, from: 2, to: 4))
         elevator.add(Passenger(id: 3, from: 3, to: 1))
-        elevator.run()
-        
-        XCTAssertTrue(elevator.requests.isEmpty, "Passengers still waiting")
-        XCTAssertTrue(elevator.passengers.isEmpty, "Passengers still inside elevator")
+        XCTAssertEqual(elevator.requests.count, 4, "Should be 4 requests")
     }
     
-    func testAssertions4() {
-        let passengers = [
-            Passenger(id: 0, from: 4, to: 3),
-            Passenger(id: 1, from: 1, to: 2),
-            Passenger(id: 2, from: 2, to: 4),
-            Passenger(id: 3, from: 3, to: 1)
-        ]
-        
+    // Elevator should be able to determine whether it needs to move up or down
+    func testAssertions6() {
         var elevator = Elevator(floors: 4)
-        // take passengers to their requested floor
-        passengers.forEach {
-            elevator.add($0)
-        }
+        elevator.add(Passenger(id: 0, from: 1, to: 2))
         elevator.run()
-        
-        // bring passengers back to their original floor
-        passengers.forEach {
-            elevator.add(Passenger(id: $0.id, from: $0.to, to: $0.from))
-        }
+        XCTAssertEqual(elevator.current, 2, "Should be on floor 2")
+        XCTAssertEqual(elevator.up, true, "Should be going up")
+        elevator.add(Passenger(id: 1, from: 2, to: 1))
         elevator.run()
-        
-        XCTAssertTrue(elevator.requests.isEmpty, "Passengers still waiting")
-        XCTAssertTrue(elevator.passengers.isEmpty, "Passengers still inside elevator")
+        XCTAssertEqual(elevator.current, 1, "Should be on floor 1")
+        XCTAssertEqual(elevator.up, false, "Should be going down")
+        elevator.add(Passenger(id: 2, from: 1, to: 3))
+        elevator.run()
+        XCTAssertEqual(elevator.current, 3, "Should be on floor 3")
+        XCTAssertEqual(elevator.up, true, "Should be going up")
     }
     
-    // future tests
-    // - test if passengers arrived at their requested floors
-    // - test if elevator changes direction based on passengers requests
+//    func testAssertions7() {
+//        var elevator = Elevator(floors: 4)
+//        elevator.add(Passenger(id: 0, from: 4, to: 3))
+//        elevator.add(Passenger(id: 1, from: 1, to: 2))
+//        elevator.add(Passenger(id: 2, from: 2, to: 4))
+//        elevator.add(Passenger(id: 3, from: 3, to: 1))
+//        elevator.run()
+//        
+//        XCTAssertTrue(elevator.requests.isEmpty, "Passengers still waiting")
+//        XCTAssertTrue(elevator.passengers.isEmpty, "Passengers still inside elevator")
+//    }
+//    
+//    func testAssertions8() {
+//        let passengers = [
+//            Passenger(id: 0, from: 4, to: 3),
+//            Passenger(id: 1, from: 1, to: 2),
+//            Passenger(id: 2, from: 2, to: 4),
+//            Passenger(id: 3, from: 3, to: 1)
+//        ]
+//        
+//        var elevator = Elevator(floors: 4)
+//        // take passengers to their requested floor
+//        passengers.forEach {
+//            elevator.add($0)
+//        }
+//        elevator.run()
+//        
+//        // bring passengers back to their original floor
+//        passengers.forEach {
+//            elevator.add(Passenger(id: $0.id, from: $0.to, to: $0.from))
+//        }
+//        elevator.run()
+//        
+//        XCTAssertTrue(elevator.requests.isEmpty, "Passengers still waiting")
+//        XCTAssertTrue(elevator.passengers.isEmpty, "Passengers still inside elevator")
+//    }
+    
 }
 
 TestCase()
